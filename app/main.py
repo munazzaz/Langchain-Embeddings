2c6056a16184867f3256be5d32786cbe8466bc87
Abdullah-Mubeen <abdulmubi11@gmail.com>
2024-12-07 21:34:51 +0500

    Integrate text preprocessing for PDF content #13

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from app.routes import users, items
+from app.routes.items import router as items_router
+
 
 app = FastAPI(
     title="Xenara FastAPI",
@@ -14,6 +16,7 @@ app = FastAPI(
 # Include routes
 app.include_router(users.router, prefix="/users", tags=["Users"])
 app.include_router(items.router, prefix="/items", tags=["Items"])
+app.include_router(items_router, prefix="/api")
 
 @app.get("/")
 async def root():
